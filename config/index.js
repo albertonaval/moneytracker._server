22ec6dad62590922bbe6ab9cdd932dd4bffe7915
albertonaval <albertonaval.dev@gmail.com>
2023-02-21 01:28:09 +0100

    Fix some problem

--- config/index.js
+++ config/index.js
@@ -17,6 +17,15 @@ module.exports = (app) => {
     })
   );
 
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+
   app.use(logger("dev"));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
