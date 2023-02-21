557ab09b558f06d9a82fa6a25518f21504086d74
albertonaval <albertonaval.dev@gmail.com>
2023-02-21 17:38:44 +0100

    Try to fix some problem with cors

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = (app) => {
   app.set("trust proxy", 1);
   app.use(
     cors({
-      origin: ["http://localhost:3000", process.env.ORIGIN],
+      origin: ["http://localhost:3000", process.env.ORIGIN, "*"],
       credentials: true
     })
   );
